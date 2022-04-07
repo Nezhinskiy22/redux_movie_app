02661777a6fb27ab10f6c1e6859dc49a6a0da7b5
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2022-04-07 14:07:07 +0300

    redux toolkit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,20 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((error) => {
           console.log("Error: ", error);
         });
-      console.log("The response from API :", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
   }, []);
